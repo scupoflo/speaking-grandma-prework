c74beb0d8a011e768e3a20256895b5d55ea9cfbc
shinik.d.cupo <shinik.d.cupo@gmail.com>
2018-11-15 18:56:16 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(phrase)
   if phrase.upcase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase.upcase == text
+  elsif phrase.upcase == phrase
     return "NO, NOT SINCE 1938!"
   else
     return "HUH?! SPEAK UP, SONNY!"
